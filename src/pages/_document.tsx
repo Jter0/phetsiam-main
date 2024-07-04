41ca771d3bb560dc3193f473abd951281a9ef4c9
Jter0 <154227222+Jter0@users.noreply.github.com>
2024-07-04 19:36:11 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-M2TK8GLR');</script>*/}
       </Head>
       <body>
-        <noscript><iframe title="Phetsiam PE Pipe" src="https://www.googletagmanager.com/ns.html?id=GTM-M2TK8GLR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/*<noscript><iframe title="Phetsiam PE Pipe" src="https://www.googletagmanager.com/ns.html?id=GTM-M2TK8GLR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>*/}
         <Main />
         <NextScript />
       </body>
