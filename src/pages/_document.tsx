e1805705af311daba5f2bbd34e4e82984c385da2
Jter0 <154227222+Jter0@users.noreply.github.com>
2024-07-04 19:39:13 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,16 +4,28 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/img/favicon.svg" type="image/x-icon" />{/*
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M2TK8GLR');</script>*/}
+        <link rel="shortcut icon" href="/img/favicon.svg" type="image/x-icon" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M2TK8GLR');
+            `,
+          }}
+        />
       </Head>
       <body>
-        {/*<noscript><iframe title="Phetsiam PE Pipe" src="https://www.googletagmanager.com/ns.html?id=GTM-M2TK8GLR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>*/}
-        <Main />
+      <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe title="Phetsiam PE Pipe" src="https://www.googletagmanager.com/ns.html?id=GTM-M2TK8GLR"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />        <Main />
         <NextScript />
       </body>
     </Html>
